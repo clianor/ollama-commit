a79ef5ab1069f30f01712146e667d000eeff1cf4
김덕현 <oraclian@estsecurity.com>
2023-12-27 22:01:28 +0900

    refactor(helpers): Refactored and simplified the helpers file by integrating Ollama model pulling into a separate function, adding progress bar for model pull, and creating a new generateCommitMessage function.
    made by ollama-commit

--- src/gpt.ts
+++ src/gpt.ts
@@ -1,54 +1,9 @@
-import { SingleBar } from "cli-progress";
-import { MODEL, PROVIDER, SIGNATURE } from "./config";
-import { defaultPromptTemplate } from "./template";
+import { SIGNATURE } from "./config";
+import { generateCommitMessage, pullModel } from "./helpers";
 
 export const generateCommit = async (diff: string): Promise<string> => {
-  const { Ollama } = await import("ollama");
-  const ollama = new Ollama();
-  const progressBar = new SingleBar({
-    format: `${PROVIDER} - ${MODEL} pulling |{bar}| {percentage}% || {value}/{total} Chunks`,
-    barCompleteChar: "\u2588",
-    barIncompleteChar: "\u2591",
-    hideCursor: true,
-  });
-
-  // 1. ollama model pulling
-  for await (const pullingResult of ollama.pull(MODEL)) {
-    progressBar.start(pullingResult.total, pullingResult.completed);
-    while (pullingResult.total !== pullingResult.completed) {
-      progressBar.increment();
-      progressBar.update(pullingResult.completed, {
-        total: pullingResult.total,
-      });
-    }
-  }
-  progressBar.stop();
-
-  // 2. ollama message generate
-  const options = { temperature: 0.9, top_k: 40, top_p: 0.7 };
-  console.log("\n========= prompting ollama... =========");
-
-  const response = await fetch("http://localhost:11434/api/generate", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      model: MODEL,
-      prompt: diff,
-      stream: false,
-      template: defaultPromptTemplate,
-      options,
-    }),
-  });
-
-  if (!response.ok)
-    throw new Error(`Failed to generate text: ${await response.text()}`);
-
-  const responseJson = await response.json();
-  let message = responseJson.response.trim();
+  await pullModel();
+  let message = await generateCommitMessage(diff);
   if (SIGNATURE) message += "\n\nmade by ollama-commit";
-  console.log(message);
-
-  console.log("\n========= prompting ai done! =========");
-
   return message;
 };
