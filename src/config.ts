fd3139ca6f194f5859558854ebd17093c30204ad
김덕현 <oraclian@estsecurity.com>
2023-12-27 21:56:08 +0900

    refactor(config): Simplified configuration handling
    Moved constants to src/constants and updated their default values.
    Removed hardcoded provider, model and language from src/config.ts
    This should allow for easier customization of the application's configuration.
    
    made by ollama-commit

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,14 @@
 import * as dotenv from "dotenv";
 
 import { getArgs } from "./helpers";
+import { DEFAULT_LANGUAGE, DEFAULT_MODEL, DEFAULT_PROVIDER } from "./constants";
 
 dotenv.config();
 
 export const args = getArgs();
 
-export const PROVIDER = "ollama";
-
-/**
- * ollma model list
- * https://ollama.ai/library
- */
-export const MODEL = args.model ?? process.env.MODEL ?? "mistral";
-
+export const PROVIDER = DEFAULT_PROVIDER;
+export const MODEL = args.model ?? process.env.MODEL ?? DEFAULT_MODEL;
 export const RESPONSE_LANGUAGE =
-  args.language ?? process.env.LANGUAGE ?? "English";
-
+  args.language ?? process.env.LANGUAGE ?? DEFAULT_LANGUAGE;
 export const SIGNATURE = args.signature ?? process.env.SIGNATURE;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,10 @@
+/**
+ * ==============================
+ * ARGS
+ * ==============================
+ */
+
 export const ARGS_PREFIX = "--";
+export const DEFAULT_PROVIDER = "ollama";
+export const DEFAULT_MODEL = "mistral";
+export const DEFAULT_LANGUAGE = "English";
