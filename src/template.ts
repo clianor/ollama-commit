79de84f9ec5be7e5198bae1c4dce66710a7d6e50
김덕현 <oraclian@estsecurity.com>
2023-12-28 00:55:07 +0900

    refactor(template): Adjusted type declarations in 'src/template.ts' for better types coverage.
    Changed import statement and function signature on lines 7-8, added missing interfaces.
    
    made by ollama-commit

--- src/template.ts
+++ src/template.ts
@@ -7,8 +7,10 @@ I want you to act as a commit message generator.
 I will provide the output through the 'git diff --staged' command, and you are to convert it into a commit message.
 I don't want any responses other than the commit message, so please only output the commit message.
 I want the first line of the commit message to always be under 10 words.
-I want the third line of the commit message and onwards to always be under 10 words.
-I want no empty lines between the third line of the commit message and onwards.
+I want each line of the commit body to be under 10 words.
+I don't want any blank lines between each line of the commit body.
+I want the scope of the commit to be an optional value or defined in one word.
+I want the body of the commit to strictly include only the changes.
 I only want one commit message to be output.
 Please output in ${RESPONSE_LANGUAGE}.
 [/INST]
