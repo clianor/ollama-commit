2c0ac30da6970c4b51c6bdb7e56a9614e001115e
김덕현 <oraclian@estsecurity.com>
2023-12-29 00:05:50 +0900

    refactor(utils/generateCommit): Removed generateCommit function and imported generateCommitMessage and pullModel from helpers instead.
    Replace generateCommit with generateCommitMessage call and remove the pullModel call in src/index.ts.
    Also, delete the generateCommit file.
    
    made by ollama-commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 import { MODEL, PROVIDER, SIGNATURE } from "./config";
-import { generateCommit } from "./utils/generateCommit";
 import { checkGitRepository } from "./utils/checkGitRepository";
 import { getDiff } from "./utils/getDiff";
 import { createCommit } from "./utils/createCommit";
 import { confirmContinue } from "./utils/confirmContinue";
+import { generateCommitMessage, pullModel } from "./helpers";
 
 console.log("COMMIT PROVIDER", PROVIDER);
 console.log("COMMIT MODEL", MODEL, "\n");
@@ -13,7 +13,8 @@ console.log("COMMIT MODEL", MODEL, "\n");
 const main = async () => {
   checkGitRepository();
   const diff = getDiff();
-  let message = await generateCommit(diff);
+  await pullModel();
+  let message = await generateCommitMessage(diff);
   console.log("\n========= prompting ollama... =========\n");
   console.log(message);
   if (SIGNATURE) message += "\n\nmade by ollama-commit";
--- src/utils/generateCommit.ts
+++
@@ -1,6 +0,0 @@
-import { generateCommitMessage, pullModel } from "../helpers";
-
-export const generateCommit = async (diff: string): Promise<string> => {
-  await pullModel();
-  return generateCommitMessage(diff);
-};
