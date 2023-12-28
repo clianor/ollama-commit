948a07cca7c65770fd08ebdeae11305753db50ab
김덕현 <oraclian@estsecurity.com>
2023-12-28 23:19:51 +0900

    refactor(template): Adjusted logic in template.ts to improve code readability and maintainability.
    Changed function name and removed unused variables.

--- src/template.ts
+++ src/template.ts
@@ -9,7 +9,7 @@ I don't want any responses other than the commit message, so please only output 
 I want the first line of the commit message to always be under 10 words.
 I want each line of the commit body to be under 10 words.
 I don't want any blank lines between each line of the commit body.
-commit message의 scope는 type 뒤에오며 괄호로 둘러싸인 코드베이스의 섹션을 설명하는 명사로 구성되어야 합니다. e.g., fix(parser)
+The scope of the commit message should come after the type and be a noun describing a section of the codebase, surrounded by parentheses. e.g., fix(parser)
 I want the body of the commit to strictly include only the changes.
 I only want one commit message to be output.
 Please output in ${RESPONSE_LANGUAGE}.
