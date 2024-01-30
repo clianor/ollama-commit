4aadd1320851b7d6d2c6514f278f2ee96b35c5e9
김덕현 <oraclian@estsecurity.com>
2024-01-30 21:43:30 +0900

    refactor: options.ts 및 Constants.ts에서 사용되지 않는 상수 'DEFAULT_MAX_DIFF_LENGTH'를 제거합니다.
    src/constants/index.ts 및 src/options.ts의 변경 사항
    'DEFAULT_MAX_DIFF_LENGTH' 상수는 더 이상 애플리케이션에서 사용되지 않습니다.
    코드 가독성과 유지 관리성을 향상시키기 위해 제거되었습니다.
    
    made by ollama-commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,4 @@
 export const PROVIDER = "ollama";
 export const DEFAULT_MODEL = "mistral";
 export const DEFAULT_LANGUAGE = "en";
-export const DEFAULT_MAX_DIFF_LENGTH = "8000";
 export const DEFAULT_API_HOST = "http://localhost:11434";
--- src/options.ts
+++ src/options.ts
@@ -1,12 +1,7 @@
 import { Command } from "commander";
 
 import { version } from "../package.json";
-import {
-  DEFAULT_API_HOST,
-  DEFAULT_LANGUAGE,
-  DEFAULT_MAX_DIFF_LENGTH,
-  DEFAULT_MODEL,
-} from "./constants";
+import { DEFAULT_API_HOST, DEFAULT_LANGUAGE, DEFAULT_MODEL } from "./constants";
 
 const program = new Command();
 program
@@ -18,11 +13,6 @@ program
     "-l, --language <language>",
     "the string of the language to translate to. It can be in any of the two ISO 639 (1 or 2) or the full name in English like Spanish.",
     DEFAULT_LANGUAGE
-  )
-  .option(
-    "--max-diff-length <maxDiffLength>",
-    "max diff length",
-    DEFAULT_MAX_DIFF_LENGTH
   )
   .option("-s, --signature")
   .option("-v, --verbose")
